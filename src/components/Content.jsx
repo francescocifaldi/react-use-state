de7557bca4c5487dd73ad12bac7195cb7b9a18eb
francescocifaldi <francesco.cifaldi@gmail.com>
2024-12-04 15:52:21 +0100

    ADD style to content

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,6 +1,13 @@
+import style from "./Content.module.css"
+
 export default function Content({ item }) {
     const { id, title, description } = item
     return (
-        <>{description}</>
+        <>
+            <div className={style.description}>
+                <h2>{title}</h2>
+                <p>{description}</p>
+            </div>
+        </>
     )
 }
